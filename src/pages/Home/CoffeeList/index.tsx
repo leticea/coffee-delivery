36bc3b08444e52b0528192073ecb6f330f47fd07
leticea <nascimento.leticeah@gmail.com>
2023-03-29 16:24:38 -0300

    feat(coffeeList): container styles

--- src/pages/Home/CoffeeList/index.tsx
+++ src/pages/Home/CoffeeList/index.tsx
@@ -1,9 +1,25 @@
-import { Container, OurCoffees } from "./styles";
+import { Container, List, OurCoffees } from "./styles";
 
 export function CoffeeList() {
   return (
     <Container>
       <OurCoffees>Nossos cafés</OurCoffees>
+      <List>
+        <li>café</li>
+        <li>café</li>
+        <li>café</li>
+        <li>café</li>
+        <li>café</li>
+        <li>café</li>
+        <li>café</li>
+        <li>café</li>
+        <li>café</li>
+        <li>café</li>
+        <li>café</li>
+        <li>café</li>
+        <li>café</li>
+        <li>café</li>
+      </List>
     </Container>
   );
 }
--- src/pages/Home/CoffeeList/styles.ts
+++ src/pages/Home/CoffeeList/styles.ts
@@ -2,13 +2,10 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   height: 164.5rem;
-  top: 648px;
-
-  border: 1px solid red;
+  //border: 1px solid red;
 `;
 
 export const OurCoffees = styled.h1`
-  position: absolute;
   min-width: 19.3rem;
   height: 4.2rem;
   font-family: "Baloo 2";
@@ -18,6 +15,18 @@ export const OurCoffees = styled.h1`
   color: ${(props) => props.theme["gray-800"]};
 
   margin: 3.2rem auto auto 16rem;
+
+  //border: 1px solid red;
+`;
+
+export const List = styled.ul`
+  height: 136rem;
+  margin: 5.4rem 16rem auto 16rem;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+
+
+  list-style: none;
 
   border: 1px solid red;
 `;
