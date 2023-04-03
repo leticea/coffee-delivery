6868380d6bb748e19abb0abc9c9b8c08c6a2e00a
leticea <nascimento.leticeah@gmail.com>
2023-04-03 12:59:10 -0300

    feat(CoffeeCard): creating image interface

--- src/pages/Home/CoffeeList/CoffeeCard/index.tsx
+++ src/pages/Home/CoffeeList/CoffeeCard/index.tsx
@@ -1,15 +1,20 @@
+import { ImgHTMLAttributes } from "react";
 
 interface CoffeeProps {
   id: number;
   tags: string;
   name: string;
   description: string;
-  image: string;
   price: number;
+}
+
+interface ImageProps extends ImgHTMLAttributes<HTMLImageElement> {
+  image: string;
 }
 
 interface CoffeeCardProps {
   coffee: CoffeeProps;
+  image: ImageProps;
 }
 
 export function CoffeeCard({ coffee }: CoffeeCardProps) {
