be798252a6ccc190930d3b082ce5fded2a71545f
leticea <nascimento.leticeah@gmail.com>
2023-04-04 14:51:10 -0300

    feat(CoffeeCard): creating increase and decrease function

--- src/pages/Home/CoffeeList/CoffeeCard/index.tsx
+++ src/pages/Home/CoffeeList/CoffeeCard/index.tsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
+
 import { ShoppingCart } from "phosphor-react";
 import { moneyFormat } from "../../../../utils/moneyFormat";
-import { Button, Buttons, CoffeesCard, Description, Footer, Name, Tags } from "./styles";
+import {
+  Button,
+  Buttons,
+  CoffeesCard,
+  Description,
+  Footer,
+  Name,
+  Tags,
+} from "./styles";
 
 interface CoffeeProps {
   id: number;
@@ -17,21 +27,33 @@ interface CoffeeCardProps {
 
 export function CoffeeCard({ coffee }: CoffeeCardProps) {
   const { id, tags, name, image, description, price } = coffee;
+
+  function QuantityButton() {
+    const [quantity, setQuantity] = useState(0);
+
+    function handleIncrease() {
+
+    }
+
+    function handleDecrease() {
+
+    }
+  }
+
   return (
     <CoffeesCard>
       <img src={image} alt="" />
       {tags.map((tag) => {
-        return <Tags>{tag}</Tags>
+        return <Tags>{tag}</Tags>;
       })}
       <Name>{name}</Name>
       <Description>{description}</Description>
       <Footer>
-        <p>R$
+        <p>
+          R$
           <span>{moneyFormat(price)}</span>
         </p>
-        <Buttons>
-            - 1 +
-        </Buttons>
+        <Buttons>- 1 +</Buttons>
         <Button>
           <ShoppingCart weight="fill" size={21} />
         </Button>
