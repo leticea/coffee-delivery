6a6bf08294f06a9268a65681c2bf818046ce3fc7
leticea <nascimento.leticeah@gmail.com>
2023-04-25 17:18:56 -0300

    feat(CoffeeList): creating addToCart function

--- src/pages/Home/CoffeeList/index.tsx
+++ src/pages/Home/CoffeeList/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { coffees } from "../../../utils/coffees";
 import { CoffeeCard } from "./CoffeeCard";
 import { Container, List } from "./styles";
@@ -15,11 +15,23 @@ interface CoffeeListProps {
 export function CoffeeList({ receiveCartValue }: CoffeeListProps) {
   const [newOrder, setNewOrder] = useState<CoffeeOrderProps[]>([]);
 
+  useEffect(() => {
+
+    if (newOrder.length !== 0) {
+      avisarAPI()
+    }
+    console.log(newOrder)
+  }, [newOrder])
+
   function addCoffeeToCart(item: CoffeeOrderProps) {
     const aux = [...newOrder, item];
 
     setNewOrder(aux);
     receiveCartValue();
+  }
+
+  function avisarAPI() {
+    console.log('Pedido feito!');
   }
 
   return (
