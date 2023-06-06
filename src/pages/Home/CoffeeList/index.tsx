64338d5fda9d67df2e5ac1e7a7ed0fea96e2b0f7
leticea <nascimento.leticeah@gmail.com>
2023-06-06 16:59:38 -0300

    feat(Home): editing

--- src/pages/Home/CoffeeList/index.tsx
+++ src/pages/Home/CoffeeList/index.tsx
@@ -3,35 +3,12 @@ import { coffees } from "../../../utils/coffees";
 import { CoffeeCard } from "./components/CoffeeCard";
 import { Container, List } from "./styles";
 
-interface CoffeeOrderProps {
-  id: number;
-  quantity: number;
-}
-
-interface CoffeeListProps {
-  receiveCartValue: () => void;
-}
-
-export function CoffeeList({ receiveCartValue }: CoffeeListProps) {
-  //const [newOrder, setNewOrder] = useState<CoffeeOrderProps[]>([]);
-
-  // function addCoffeeToCart(item: CoffeeOrderProps) {
-  //   const aux = [...newOrder, item];
-  //   setNewOrder(aux);
-  //   receiveCartValue();
-  // }
-
+export function CoffeeList() {
   return (
     <Container>
       <List>
         {coffees.map((coffee) => {
-          return (
-            <CoffeeCard
-              key={coffee.id}
-              coffee={coffee}
-              // addCoffeeToCart={addCoffeeToCart}
-            />
-          );
+          return <CoffeeCard key={coffee.id} coffee={coffee} />;
         })}
       </List>
     </Container>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,13 @@
 import { CoffeeList } from "./CoffeeList";
 import { Introduction } from "./Introduction";
 import { OurCoffees } from "./CoffeeList/components/OurCoffees";
-//import { CoffeeProps } from "./CoffeeList/components/CoffeeCard";
 
 export function Home() {
   return (
     <>
       <Introduction />
       <OurCoffees />
-      <CoffeeList
-        receiveCartValue={function (): void {
-          throw new Error("Function not implemented.");
-        }}
-      />
+      <CoffeeList />
     </>
   );
 }
