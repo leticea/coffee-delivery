3d6e858b0495ea49137ade2dac18be77333d7aec
leticea <nascimento.leticeah@gmail.com>
2023-06-22 09:26:59 -0300

    feat(Input): creating Input Component styles

--- src/pages/Cart/components/CheckoutForm/index.tsx
+++ src/pages/Cart/components/CheckoutForm/index.tsx
@@ -2,8 +2,17 @@ import { MapPinLine } from "phosphor-react";
 import { InfoContainer, Headline, AddressForm } from "./styles";
 import { useFormContext } from "react-hook-form";
 
+interface ErrorsType {
+  errors: {
+    [key: string]: {
+      message: string;
+    };
+  };
+}
+
 export function CheckoutForm() {
   const { register, formState } = useFormContext();
+  const { errors } = formState as unknown as ErrorsType;
 
   console.log(formState.errors);
 
@@ -24,12 +33,14 @@ export function CheckoutForm() {
             type="number"
             placeholder="CEP"
             {...register("cep")}
+            error={errors.cep?.message}
           />
           <input
             className="street"
             type="text"
             placeholder="Rua"
             {...register("street")}
+            error={errors.street?.message}
           />
 
           <section>
@@ -38,12 +49,15 @@ export function CheckoutForm() {
               type="number"
               placeholder="Número"
               {...register("number")}
+              error={errors.number?.message}
             />
             <input
               className="complement"
               type="text"
               placeholder="Complemento"
               {...register("complement")}
+              error={errors.complement?.message}
+              rightText="Opcional"
             />
           </section>
 
@@ -53,18 +67,21 @@ export function CheckoutForm() {
               type="text"
               placeholder="Bairro"
               {...register("district")}
+              error={errors.district?.message}
             />
             <input
               className="city"
               type="text"
               placeholder="Cidade"
               {...register("city")}
+              error={errors.city?.message}
             />
             <input
               className="state"
               type="text"
               placeholder="UF"
               {...register("state")}
+              error={errors.uf?.message}
             />
           </section>
         </AddressForm>
